ad0f38ebb3bbde47f15dcfb5fde6a38b7776fe69
Matflo <mattfloclato@gmail.com>
2022-04-25 16:58:33 +0200

    terre03.ts

--- src/app/hello.ts
+++ src/app/hello.ts
@@ -2,6 +2,6 @@ function alphabett(person: string){
   return "Hello, " + person;
 }
 let user = "Aamod Tiwari";
-const result = alphabett(user);
-console.log("Result", result)
+const r = alphabett(user);
+console.log("Result", r)
 
--- src/app/terre02.ts
+++ src/app/terre02.ts
@@ -1,2 +1,6 @@
 var args = process.argv.slice(2);
   console.log(args.toString().split(',').join(' \n'));
+console.log('--------------------------------------');
+process.argv.forEach(function (val) {
+  console.log(val);
+});
