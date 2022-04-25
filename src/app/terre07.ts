d4af4923266c512ca89711e29c2a541a77b33e90
Matflo <mattfloclato@gmail.com>
2022-04-25 21:01:44 +0200

    terre05-06-07-08.ts

---
+++ src/app/terre07.ts
@@ -0,0 +1,3 @@
+var args = process.argv.slice(2);
+
+console.log(args.toString().length)
---
+++ src/app/terre08.ts
@@ -0,0 +1,8 @@
+var args = process.argv.slice(2);
+
+if(true){
+  console.log("resultat : " +  Math.floor(parseInt(args[0].toString()) ** parseInt(args[1].toString())));
+  console.log("resultat : " +  Math.pow(parseInt(args[0].toString()), parseInt(args[1].toString())));
+}else{
+  console.log("erreur");
+}
